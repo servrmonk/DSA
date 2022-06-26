1eaa3ed86e1a791c0d4e85ceeec3847d039d0b11
raushan2409 <88944944+raushan2409@users.noreply.github.com>
2022-06-26 12:21:28 +0530

    Add files via upload

---
+++ counting_the_nodes.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node{
+    int data;
+    struct node *link;
+};
+void count_of_nodes(struct node *head)
+{
+    int count = 0;
+    if(head==NULL)
+    printf("Linked list is empty ");
+    struct node *ptr = NULL;
+    ptr = head;
+    while(ptr!=NULL)
+    {
+        count++;
+        ptr = ptr->link;
+    }
+    printf("%d ",count);
+}
+int main()
+{
+    struct node *head;
+    count_of_nodes(head);
+    return 0;
+}
